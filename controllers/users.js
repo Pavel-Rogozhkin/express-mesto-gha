ee7176795a42ef5e39550da013548c1b8478b1fd
Pavel Rogozhkin <rogozhkinpaul@yandex.ru>
2022-09-09 22:51:41 +0300

    play with create User 1

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,21 @@
-const createUser = (req, res) => {
-  res.send(req.body);
+const User = require('../models/user');
+
+// const createUser = (req, res) => {
+//   console.log(req.body);
+//   res.send(req.body);
+// };
+
+const createUser = async (req, res) => {
+  try {
+    const user = await new User(req.body).save();
+
+    return res.status(200).send(user);
+  } catch (e) {
+    if (e.name === 'ValidationError') {
+      return res.status(400).send({ message: 'Переданы неккоректные данные', ...e });
+    }
+    return res.status(500).send({ message: 'Произошла ошибка на сервере', ...e });
+  }
 };
 
 const getUser = (req, res) => {
