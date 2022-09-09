697a7dfc348353dccd9116c0420c806532387c09
Pavel Rogozhkin <rogozhkinpaul@yandex.ru>
2022-09-09 21:38:59 +0300

    add controller users to route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
+const { createUser } = require('../controllers/users');
+
 const usersRoutes = express.Router();
 
 usersRoutes.get('/users', express.json());
 usersRoutes.get('/users/:userId', express.json());
-usersRoutes.post('/users', express.json());
+usersRoutes.post('/users', express.json(), createUser);
 
 module.exports = { usersRoutes };
