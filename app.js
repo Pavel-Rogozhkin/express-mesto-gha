7ffde04b4913e37a46fda7c774887befacb238f7
Rogozhkin Pavel <rogozhkin_pa@rtsoft.ru>
2022-09-14 11:53:59 +0300

    add posts '/signin' and '/signup'

--- app.js
+++ app.js
@@ -3,10 +3,28 @@ const mongoose = require('mongoose');
 const console = require('console');
 const { usersRoutes } = require('./routes/users');
 const { cardsRoutes } = require('./routes/cards');
+const { celebrate, Joi } = require('celebrate');
+const { createNewUser, login } = require('./controllers/users');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(8).required(),
+  }),
+}), login,);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().regex(/^(https?:\/\/)?([\w\.]+)\.([a-z]{2,6}\.?)(\/[\w\.]*)*\/?$/),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(8).required(),
+  }),
+}), createNewUser,); 
 
 app.use((req, res, next) => {
   req.user = {
@@ -14,9 +32,11 @@ app.use((req, res, next) => {
   };
   next();
 });
+
 app.use(express.json());
 app.use(usersRoutes);
 app.use(cardsRoutes);
+
 app.use((req, res) => {
   res.status(404).send({ message: 'Страница не найдена' });
 });
