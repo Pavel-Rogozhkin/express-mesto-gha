b859441c3c7305c0432be39cecfb353de1f5e1e2
Rogozhkin Pavel <rogozhkin_pa@rtsoft.ru>
2022-09-14 10:48:50 +0300

    add auth mw

---
+++ middlewares/auth.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+
+const auth = async (req, res, next) => {
+  let payload;
+  const token = req.cookies.jwt;
+  try {
+    payload = await jwt.verify(token, 'Enigma');
+  } catch (error) {
+    return res.status(401).send({ message: 'Требуется авторизация' });
+  }
+  req.user = payload;
+  next();
+};
+
+module.exports = { auth };
