bf45997e8ad0513be9beaa5c4168b61c9511f465
Rogozhkin Pavel <rogozhkin_pa@rtsoft.ru>
2022-09-15 16:33:56 +0300

    some exp fix 3

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.use((err, req, res, next) => {
 });
 
 async function server() {
-  mongoose.connect('mongodb://localhost:27017/mestodb', {
+  await mongoose.connect('mongodb://localhost:27017/mestodb', {
     useNewUrlParser: true,
     useUnifiedTopology: false,
   });
