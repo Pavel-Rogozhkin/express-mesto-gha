75be5e49b7a2f648c252e32b46ea022bec229b74
Rogozhkin Pavel <rogozhkin_pa@rtsoft.ru>
2022-09-15 18:36:24 +0300

    bugfix 18

--- app.js
+++ app.js
@@ -16,7 +16,11 @@ const { PORT = 3000 } = process.env;
 const SERVER_CODE = 500;
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  }),
+);
 app.use(bodyParser.json());
 app.use(cookieParser());
 
