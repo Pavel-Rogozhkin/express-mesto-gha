1fb7d234388bd60a8ee8728cea371741f87ee605
Rogozhkin Pavel <rogozhkin_pa@rtsoft.ru>
2022-09-15 18:15:32 +0300

    bugfix 14

--- routes/users.js
+++ routes/users.js
@@ -26,12 +26,12 @@ usersRoutes.get(
 
 usersRoutes.get(
   '/users/me',
-  celebrate({
-    body: Joi.object().keys({
-      name: Joi.string().min(2).max(30),
-      about: Joi.string().min(2).max(30),
-    }),
-  }),
+  // celebrate({
+  //   body: Joi.object().keys({
+  //     name: Joi.string().min(2).max(30),
+  //     about: Joi.string().min(2).max(30),
+  //   }),
+  // }),
   getMainUserInfo,
 );
 
