9cb66f4d8688473720b22ffa98e099fbae456497
Rogozhkin Pavel <rogozhkin_pa@rtsoft.ru>
2022-09-15 16:47:05 +0300

    valid users

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,6 @@
+/* eslint-disable import/no-unresolved */
 const express = require('express');
+const { celebrate, Joi } = require('celebrate');
 
 const {
   getUsers,
@@ -11,9 +13,34 @@ const {
 const usersRoutes = express.Router();
 
 usersRoutes.get('/users', getUsers);
-usersRoutes.get('/users/:userId', getUserById);
+usersRoutes.get(
+  '/users/:userId',
+  celebrate({
+    params: Joi.object().keys({
+      userId: Joi.string().length(24).alphanum(),
+    }),
+  }),
+  getUserById,
+);
 usersRoutes.get('/users/me', getMainUserInfo);
-usersRoutes.patch('/users/me', updateMainUser);
-usersRoutes.patch('/users/me/avatar', updateMainUserAvatar);
+usersRoutes.patch(
+  '/users/me',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+    }),
+  }),
+  updateMainUser,
+);
+usersRoutes.patch(
+  '/users/me/avatar',
+  celebrate({
+    body: Joi.object().keys({
+      avatar: Joi.string().regex(/^(https?:\/\/)?([\w\\.]+)\.([a-z]{2,6}\.?)(\/[\w\\.]*)*\/?$/),
+    }),
+  }),
+  updateMainUserAvatar,
+);
 
 module.exports = { usersRoutes };
